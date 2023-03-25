963f8fd2ce33468f2d89eb6fc95dc2c26b86a68a
Mohamed A. Bahrawy <mbahrawy@tendegrees.sa>
2023-03-25 21:23:51 +0200

    cleaning

--- src/components/Comments/Comments.component.tsx
+++ src/components/Comments/Comments.component.tsx
@@ -29,30 +29,6 @@ function Comments({ data }: CommentsProps): JSX.Element {
                   </div>
                 );
               })}
-
-              <div className="card-footer py-3 border-0">
-                <div className="d-flex flex-start w-100">
-                  <img className="rounded-circle shadow-1-strong me-3" src={UserIcon} alt="avatar" width="40" height="40" />
-                  <div className="form-outline w-100 d-flex flex-wrap justify-content-between gap-2">
-                    <textarea className="form-control w-100" id="textAreaExample" rows={4} />
-
-                    <div className="">
-                      <label className="form-label" htmlFor="textAreaExample">
-                        leave a coment
-                      </label>
-                    </div>
-
-                    <div className="d-flex gap-2">
-                      <button type="button" className="btn btn-primary btn-sm">
-                        Post comment
-                      </button>
-                      <button type="button" className="btn btn-outline-primary btn-sm">
-                        Cancel
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              </div>
             </div>
           </div>
         </div>
